f7eb058666a02008439fd26e88a5f0e5eb96e411
pbutti <pierfrancesco.butti@gmail.com>
2025-01-22 12:54:07 +0100

    update to script to run nproc

--- run_thread_scaling.py
+++ run_thread_scaling.py
@@ -15,9 +15,17 @@ def prepareJobScript(outfolder,nthreads,args):
 
         f.write("#!/usr/bin/bash\n\n\n")
         f.write('ignore_pattern="Acts.+FindingAlg.+ERROR.+Propagation.+reached.+the.+step.+count.+limit,Acts.+FindingAlg.+ERROR.+Propagation.+failed:.+PropagatorError:3.+Propagation.+reached.+the.+configured.+maximum.+number.+of.+steps.+with.+the.+initial.+parameters,Acts.+FindingAlg.Acts.+ERROR.+CombinatorialKalmanFilter.+failed:.+CombinatorialKalmanFilterError:5.+Propagation.+reaches.+max.+steps.+before.+track.+finding.+is.+finished.+with.+the.+initial.+parameters,Acts.+FindingAlg.Acts.+ERROR.+SurfaceError:1,Acts.+FindingAlg.Acts.+ERROR.+failed.+to.+extrapolate.+track"\n\n')
-        f.write("DATADIR=\"/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/PhaseIIUpgrade/RDO\" \n\n")
+        #f.write("DATADIR=\"/cvmfs/atlas-nightlies.cern.ch/repo/data/data-art/PhaseIIUpgrade/RDO\" \n\n")  #Single File
+
+        #Full PU Truth, larger file, higher IO, slower processing time, PU200
+        #f.write("DATADIR=\"/home/pibutti/data/ttbar_PU200/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14701\" \n\n")
+
+        #HS PU Only, smaller file, lower IO, faster processing time, PU200
+        f.write("DATADIR=\"/home/pibutti/data/ttbar_PU200/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700\" \n\n")
+        
         f.write("export TRF_ECHO=1 \n")
         f.write("export ATHENA_CORE_NUMBER="+str(nthreads)+" \n")
+        
         f.write('echo "ATHENA_CORE_NUMBER=" ${ATHENA_CORE_NUMBER} \n\n\n')
 
         f.write("cd "+outfolder+"\n\n")
@@ -26,6 +34,10 @@ def prepareJobScript(outfolder,nthreads,args):
         
         preInclude = "--preInclude 'InDetConfig.ConfigurationHelpers.OnlyTrackingPreInclude' \\\n"
         preExec    = "--preExec 'all:ConfigFlags.Tracking.doITkFastTracking=True' \\\n"
+        multiproc  = ""
+
+        if args.nproc > 1:
+            multiproc="--athenaopts=\"--nprocs="+str(args.nproc)+"\" \\\n"
 
         #For ACTS
         if not legacy:
@@ -42,10 +54,11 @@ def prepareJobScript(outfolder,nthreads,args):
                 + "--steering 'doRAWtoALL' \\\n"
                 + preExec
                 + "--postExec 'all:cfg.getService(\"AlgResourcePool\").CountAlgorithmInstanceMisses = True;' \\\n"
-                + "--inputRDOFile ${DATADIR}\"/ATLAS-P2-RUN4-03-00-00/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700/*\" \\\n"
+                + "--inputRDOFile ${DATADIR}\"/*\" \\\n"
                 +"--outputAODFile 'myAOD.pool.root' \\\n"
                 +"--jobNumber '1' \\\n"
                 +"--ignorePatterns \"${ignore_pattern}\" \\\n"
+                + multiproc
                 +"--multithreaded \n\n\n")
         
         f.write("cd -")
@@ -64,9 +77,21 @@ def geo_progression(start, ratio=2, limit=48):
 
 
 # /software/modules/sw/core/likwid/5.2/bin/likwid-topology
-def make_thread_list(n, smt=True):
+# p is the process number, n is the thread number
+def make_thread_list(p, n, smt=True):
 
     thread_lists = []
+
+    #if number of processes is set as <=0 by mistake, put it to 1 
+    if p<=0:
+        p=1
+
+    # total threads are split in between processes and 
+    total_threads = p * n
+
+    if (n > 24):
+        print("Running with processes=",p,"and threads=",n," is not supported because is more than 24 threads in parallel")
+        sys.exit(1)
     
     if smt:
         thread_lists = list(itertools.chain.from_iterable([(6 * j + i) % 48, (6 * j + i + 24) % 48] for i in range(6) for j in range(4)))
@@ -74,7 +99,7 @@ def make_thread_list(n, smt=True):
     else:
         thread_lists = [(6 * j + i) % 48 for i in range(6) for j in range(4)]
                            
-    threads_str  = ",".join(str(x) for x in thread_lists[:n])
+    threads_str  = ",".join(str(x) for x in thread_lists[:total_threads])
     
     
     return(threads_str)
@@ -84,24 +109,12 @@ def runJob(jobfile,threads, args):
 
     nosmt=args.nosmt
     subprocess.run(['chmod','u+x',jobfile])
-    threads_str = make_thread_list(threads, not nosmt)
+    threads_str = make_thread_list(args.nproc, threads, not nosmt)
     
     
     command=['taskset','-c',threads_str,jobfile]
     print(command)
-
-        
-    #subprocess.run(command)
-        
-    #else:
-    #    threadlim="0"
-    #    
-    #    if (threads > 1):
-    #        threadlim="0-"+str(threads-1)
-            
-    #    command=['taskset','-c',threads_str,jobfile]
-    #    print(command)
-
+    
     if not args.dry:
         subprocess.run(command)
 
@@ -129,6 +142,8 @@ def main():
     parser.add_argument("-r", "--runs",type=int, required=False, default=1,help="Number of runs for each data point")
 
     parser.add_argument("-l", "--legacy", required=False, default=False, action="store_true", help="Run legacy tracking instead")
+
+    parser.add_argument("-p", "--nproc", required=False, default=1, type=int, help="How many processes to run with")
     
     # Parse the arguments
     args = parser.parse_args()
@@ -143,16 +158,16 @@ def main():
 
     #For each thread configuration make a folder, setup the RecoTf script and run
 
+        
     for tc in threads:
         
-        outfolder = path+"athena_threads_"+str(tc)
-        
+        outfolder = path+"athena_proc_"+str(args.nproc)+"_threads_"+str(tc)
+
         if (args.legacy):
             outfolder = outfolder+"_legacy"
             
         if (args.nosmt):
             outfolder = outfolder+"_nosmt"
-            
 
         if not os.path.exists(outfolder):
             os.makedirs(outfolder)
