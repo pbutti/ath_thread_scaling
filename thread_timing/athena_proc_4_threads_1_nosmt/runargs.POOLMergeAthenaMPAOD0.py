31f7010a83f83f4f58d6ee7791a3ba22a8f17d4d
pbutti <pierfrancesco.butti@gmail.com>
2025-01-23 10:17:49 +0100

    proc 2 and 4

---
+++ thread_timing/athena_proc_4_threads_1_nosmt/runargs.POOLMergeAthenaMPAOD0.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env athena.py
+# Run arguments file auto-generated on Wed Jan 22 19:14:45 2025 by:
+# JobTransform: POOLMergeAthenaMPAOD0
+# Version: $Id: trfExe.py 792052 2017-01-13 13:36:51Z mavogel $
+# Import runArgs class
+from PyJobTransforms.trfJobOptions import RunArguments
+runArgs = RunArguments()
+runArgs.trfSubstepName = 'POOLMergeAthenaMPAOD0' 
+
+runArgs.perfmon = 'fastmonmt'
+runArgs.autoConfiguration = ['everything']
+runArgs.conditionsTag = 'OFLCOND-MC21-SDR-RUN4-02'
+runArgs.geometryVersion = 'ATLAS-P2-RUN4-03-00-00'
+runArgs.postInclude = ['PyJobTransforms.UseFrontier']
+runArgs.preInclude = ['InDetConfig.ConfigurationHelpers.OnlyTrackingPreInclude', 'ActsConfig.ActsCIFlags.actsAloneFastWorkflowFlags']
+runArgs.postExec = ['cfg.getService("AlgResourcePool").CountAlgorithmInstanceMisses = True;']
+runArgs.jobNumber = 1
+runArgs.multithreaded = True
+
+# Explicitly added to process all events in this step
+runArgs.maxEvents = -1
+
+ # Input data
+runArgs.inputPOOL_MRG_INPUTFile = ['myAOD.pool.root_001', 'myAOD.pool.root_002', 'myAOD.pool.root_003', 'myAOD.pool.root_004']
+runArgs.inputPOOL_MRG_INPUTFileType = 'AOD'
+runArgs.inputPOOL_MRG_INPUTFileNentries = 2400
+runArgs.POOL_MRG_INPUTFileIO = 'input'
+
+ # Output data
+runArgs.outputPOOL_MRG_OUTPUTFile = 'myAOD.pool.root'
+runArgs.outputPOOL_MRG_OUTPUTFileType = 'AOD'
+
+ # Extra runargs
+
+ # Extra runtime runargs
+
+ # Literal runargs snippets
+
+ # Executor flags
+runArgs.totalExecutorSteps = 0
+
+ # Threading flags
+runArgs.nprocs = 0
+runArgs.threads = 0
+runArgs.concurrentEvents = 0
+
+ # Import skeleton and execute it
+from RecJobTransforms.MergePool_Skeleton import fromRunArgs
+fromRunArgs(runArgs)
---
+++ thread_timing/athena_proc_4_threads_1_nosmt/runwrapper.POOLMergeAthenaMPAOD0.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# AthenaMT explicitly disabled for this executor
+# AthenaMP explicitly disabled for this executor
+athena.py --preloadlib=$ATLASMKLLIBDIR_PRELOAD/libintlc.so.5:$ATLASMKLLIBDIR_PRELOAD/libimf.so runargs.POOLMergeAthenaMPAOD0.py
