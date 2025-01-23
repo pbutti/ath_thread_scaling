86e00afbd240e68e7951ab0b93f3ea6e9c8aeaf7
pbutti <pierfrancesco.butti@gmail.com>
2025-01-23 10:17:49 +0100

    proc 2 and 4

---
+++ thread_timing/athena_proc_4_threads_1_nosmt/runargs.RAWtoALL.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env athena.py
+# Run arguments file auto-generated on Wed Jan 22 18:42:01 2025 by:
+# JobTransform: RAWtoALL
+# Version: $Id: trfExe.py 792052 2017-01-13 13:36:51Z mavogel $
+# Import runArgs class
+from PyJobTransforms.trfJobOptions import RunArguments
+runArgs = RunArguments()
+runArgs.trfSubstepName = 'RAWtoALL' 
+
+runArgs.perfmon = 'fastmonmt'
+runArgs.maxEvents = 4000
+runArgs.autoConfiguration = ['everything']
+runArgs.conditionsTag = 'OFLCOND-MC21-SDR-RUN4-02'
+runArgs.geometryVersion = 'ATLAS-P2-RUN4-03-00-00'
+runArgs.postInclude = ['PyJobTransforms.UseFrontier']
+runArgs.preInclude = ['InDetConfig.ConfigurationHelpers.OnlyTrackingPreInclude', 'ActsConfig.ActsCIFlags.actsAloneFastWorkflowFlags']
+runArgs.postExec = ['cfg.getService("AlgResourcePool").CountAlgorithmInstanceMisses = True;']
+runArgs.jobNumber = 1
+runArgs.multithreaded = True
+
+ # Input data
+runArgs.inputRDOFile = ['/home/pibutti/data/ttbar_PU200/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700/RDO.33629020._000002.pool.root.1', '/home/pibutti/data/ttbar_PU200/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700/RDO.33629020._000007.pool.root.1', '/home/pibutti/data/ttbar_PU200/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700/RDO.33629020._000008.pool.root.1', '/home/pibutti/data/ttbar_PU200/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700/RDO.33629020._000014.pool.root.1', '/home/pibutti/data/ttbar_PU200/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700/RDO.33629020._000016.pool.root.1', '/home/pibutti/data/ttbar_PU200/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700/RDO.33629020._000017.pool.root.1', '/home/pibutti/data/ttbar_PU200/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700/RDO.33629020._000018.pool.root.1', '/home/pibutti/data/ttbar_PU200/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700/RDO.33629020._000027.pool.root.1', '/home/pibutti/data/ttbar_PU200/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700/RDO.33629020._000031.pool.root.1', '/home/pibutti/data/ttbar_PU200/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700/RDO.33629020._000036.pool.root.1', '/home/pibutti/data/ttbar_PU200/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700/RDO.33629020._000037.pool.root.1', '/home/pibutti/data/ttbar_PU200/mc21_14TeV.601229.PhPy8EG_A14_ttbar_hdamp258p75_SingleLep.recon.RDO.e8481_s4149_r14700/RDO.33629020._000038.pool.root.1']
+runArgs.inputRDOFileType = 'RDO'
+runArgs.inputRDOFileNentries = 2400
+runArgs.RDOFileIO = 'input'
+
+ # Output data
+runArgs.outputAODFile = 'myAOD.pool.root_000'
+runArgs.outputAODFileType = 'AOD'
+
+ # Extra runargs
+
+ # Extra runtime runargs
+
+ # Literal runargs snippets
+
+ # AthenaMP Options. nprocs = 4
+runArgs.athenaMPWorkerTopDir = 'athenaMP-workers-RAWtoALL-r2a'
+runArgs.athenaMPOutputReportFile = 'athenaMP-outputs-RAWtoALL-r2a'
+runArgs.athenaMPEventOrdersFile = 'athenamp_eventorders.txt.RAWtoALL'
+runArgs.athenaMPCollectSubprocessLogs = True
+runArgs.athenaMPStrategy = 'SharedQueue'
+
+ # Executor flags
+runArgs.totalExecutorSteps = 0
+
+ # Threading flags
+runArgs.nprocs = 4
+runArgs.threads = 1
+runArgs.concurrentEvents = 1
+
+ # Import skeleton and execute it
+from RecJobTransforms.RAWtoALL_Skeleton import fromRunArgs
+fromRunArgs(runArgs)
