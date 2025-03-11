09d31226e08fd97191642f4758ec8ac3955544d9
Pierfrancesco <pf@pbutti-m3.dyndns.cern.ch>
2025-03-11 11:41:25 +0100

    plot and updates

--- plot_thread_scaling.py
+++ plot_thread_scaling.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+
 from common import io
 import numpy as np
 import argparse
 import glob,sys
+
 
 from collections import OrderedDict
 
@@ -68,16 +72,29 @@ def collectProcData(folderList,args):
     results = {}
 
     #print("collectProcData",folderList)
+    
     nproc=1
     nthreads=1
+
+    logdata[nproc] = {}
     
     for fld in folderList:
         fldsplit = fld.split("_")
-        nthreads = int(fldsplit[-2])
-        nproc    = int(fldsplit[-4])
-        logdata[nproc] = {}
-        logdata[nproc][nthreads]=glob.glob(fld+"/athenaMP-workers-RAWtoALL-r2a/worker*/AthenaMP.log")
 
+        if "smt" not in fldsplit:
+            nthreads = int(fldsplit[-1])
+            nproc    = int(fldsplit[-3])
+        else:
+            nthreads = int(fldsplit[-2])
+            nproc    = int(fldsplit[-4])
+
+        if not args.doThreads:
+            logdata[nproc] = {}
+            logdata[nproc][nthreads]=glob.glob(fld+"/athenaMP-workers-RAWtoALL-r2a/worker*/AthenaMP.log")
+        else:
+
+            logdata[nproc][nthreads]=glob.glob(fld+"/log.RAWtoALL_run_0")
+        
 
     # Step 1: Sort the outer dictionary by nproc
     sorted_data = OrderedDict(sorted(logdata.items()))
@@ -220,7 +237,7 @@ def analyseAthenaMT(folders,args):
     #ax1.set_ylim(0, 15)
 
     #Add a straight line with slope = troughput for 1 thread (0.313s)
-    ideal = 0.313*threads
+    ideal = 0.31*threads
 
     
     #ax2 = ax1.twinx()
@@ -265,14 +282,25 @@ def analyseAthenaMP(folders,args):
             else:
                 dir_smt.append(folder)
 
-    print("collecting proc data from",dir_nosmt)
+    print("collecting proc data from")
     
-    nosmtResults = collectProcData(dir_nosmt,args)
+    print("NO SMT")
+    print(dir_nosmt)
+    print("SMT")
+    print(dir_smt)
+    
+    
+    #nosmtResults = collectProcData(dir_nosmt,args)
+    
+    smtResults = collectProcData(dir_smt,args)
 
+    print("SMT RESULTS")
+    print(smtResults)
     
-    procs, threads, throughputMP = getProcScatterPoints(nosmtResults,2)
+    procs, threads, throughputMP = getProcScatterPoints(smtResults,2)
     print(procs,threads,throughputMP)
     
+    return procs,threads,throughputMP
     
 def main():
 
@@ -284,45 +312,113 @@ def main():
 
     parser.add_argument("-a","--avg",required=False,default=False,action="store_true",help="If there are multiple logs for the same point, do the average")
 
+    parser.add_argument("-t","--doThreads",required=False,default=False,action="store_true",help="Process thread logs or process logs")
+    
+    parser.add_argument("-d","--dry",required=False,default=False,action="store_true",help="Dry run")
+
     
     args = parser.parse_args()
     
     folders = glob.glob(args.inputdir+"/*proc*threads*")
-    
-    analyseAthenaMP(folders,args)
 
-    folders = glob.glob(args.inputdir+"/*threads*")
-    sel_folders = []
+    print("Procesing folders...")
+    print(folders)
+
+    procs, threads, throughput  = analyseAthenaMP(folders,args)
+
+    #folders = glob.glob(args.inputdir+"/*threads*")
+    #sel_folders = []
     
-    for fld in folders:
-        if "proc" not in fld:
-            sel_folders.append(fld)
+    #for fld in folders:
+    #    if "proc" not in fld:
+    #        sel_folders.append(fld)
             
-    threads, ideal,throughput,throughput_nosmt,throughput_smt_legacy,throughput_nosmt_legacy = analyseAthenaMT(sel_folders,args)
+    #threads, ideal,throughput,throughput_nosmt,throughput_smt_legacy,throughput_nosmt_legacy = analyseAthenaMT(sel_folders,args)
     
-
-    MP = np.array([0.,0.,0.,2.462, 4.648, 6.457])
-    
-
+    threads = np.array([ 1  2  4  8 16 24])
+    MP = np.array([0.307 ,0.621 ,1.241 ,2.462, 4.648, 6.457])
+    MT = np.array([0.302 ,0.597 ,1.099 ,2.002, 3.4  , 4.218])
+    ideal = 0.307*threads
     #Combined plot    
 
-    io.mtlibPlot(threads,[ideal,throughput,throughput_nosmt,throughput_smt_legacy,throughput_nosmt_legacy,MP],
-                 titleX="n threads [ AMD EPYC 7413 24-Core / 48-threads ] ",titleY="Throughput Evt/s",
+    io.mtlibPlot(threads,[ideal,MT,MP],
+                 titleX="n threads / n proc [ AMD EPYC 7413 24-Core / 48-threads ] ",titleY="Throughput Evt/s",
                  xlim=[0,24],
-                 legends=["Linear Scaling","Acts Tracking SMT ON","Acts Tracking SMT OFF","Legacy Tracking SMT ON","Legacy Tracking SMT OFF","MP"],
+                 legends=["Linear Scaling","Acts Tracking MT", "Acts Tracking MP"],
                  styles=["--","-","-","-","-","-"],colors=['blue','orange','green','red','indigo','black']
                  )
 
+    plt.savefig("thread_nproc_amd_scaling.pdf", format="pdf")
+
+
+
+
+    #ARM
     
+    #MP=np.array([0.124, 3.956,  7.822, 14.997, 20.714])
+    #threads =np.array([ 1., 32.,  64., 128., 192.])
+    #ideal = 0.124*threads
+
+
+    #io.mtlibPlot(threads,[ideal,MP],
+    #             titleX="n threads [ ARM UnicornMachine 192-Core] ",titleY="Throughput Evt/s",
+    #             xlim=[0,200],
+    #             legends=["Linear Scaling","Athena with Acts FT MP"],
+    #             styles=["--","-","-","-","-","-"],colors=['blue','orange','green','red','indigo','black'] )
         
-    plt.savefig("my_plot.pdf", format="pdf")
+    #plt.savefig("arm_scaling.pdf", format="pdf")
+
+    if not args.dry:
     
-    
+        threads = np.array([  1,   2,   4,   8,  16,  32,  64, 128])
+        ARM_MT      = np.array([0.131, 0.222, 0.365, 0.52,  0.562, 0.553, 0.533, 0.517])
+        ideal_t = 0.131*threads
+
+        procs  = np.array([  1. ,   32.,  64. , 128.  , 192.])
+        ARM_MP = np.array([0.131,   3.956, 7.822, 14.997, 20.714])
+
+        ideal   = 0.131*procs
+        
+        #io.mtlibPlot([procs,threads,procs],[ideal,ARM_MT,ARM_MP],
+        #             titleX="n threads [ ARM UnicornMachine 192-Core] ",titleY="Throughput Evt/s",
+        #             xlim=[0,200],
+        #             legends=["Linear Scaling","Athena with Acts FT MT"],
+        #             styles=["--","-","-","-","-","-"],colors=['blue','orange','green','red','indigo','black'] )
 
 
-    
-    
+        fig = plt.figure()
+        gs = fig.add_gridspec(2, 1, height_ratios=[3, 1])
+        ax1 = fig.add_subplot(gs[0, 0])
 
+        ax1.grid(True)
+        ax1.xaxis.set_major_locator(MaxNLocator(nbins=12))
+        ax1.plot(threads,ARM_MT,linestyle="-",label="Athena with Acts FT MT",color="blue")
+        ax1.plot(procs,ARM_MP,  linestyle="-",label="Athena with Acts FT MP",color="green")
+        ax1.plot(procs,ideal, linestyle="--",label="Linear Scaling",color="black")
+        
+        ax1.legend()
+        ax1.set_ylabel("Throughput Evt/s")
+        ax1.set_xlabel("Processes / Threads")
+        
+        ax2 = fig.add_subplot(gs[1, 0])
+        ax2.xaxis.set_major_locator(MaxNLocator(nbins=12))
+        ax2.set_yticks(np.arange(0, 1.25, 0.25))
+        ax2.grid(True)
+
+        # Adjust layout to prevent overlap
+        fig.subplots_adjust(hspace=0.3)  # Increase space between subplots
+        
+        
+    
+        ratio_t = ARM_MT / ideal_t
+        ratio_p = ARM_MP / ideal
+                
+        ax2.plot(threads,ratio_t,color="blue")
+        ax2.plot(procs,ratio_p,color="green")
+
+
+        plt.savefig("arm_scaling.pdf", format="pdf")
+        
 if __name__=="__main__":
     main()
 
